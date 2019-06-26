dc86e3671108611e1045879bc3a081c365bce005
Andrell Bower <bowera@gmail.com>
2019-06-26 16:22:26 -0700

    Fixing gid task structure

--- poseidon/dags/get_it_done/gid_dags.py
+++ poseidon/dags/get_it_done/gid_dags.py
@@ -146,14 +146,14 @@ for i in services:
         #: get_task must run before sonar potholes
         get_task.set_downstream(create_potholes_sonar)
 
-filename = conf['prod_data_dir'] + "/get_it_done_*.csv"
+filename = conf['prod_data_dir'] + "/get_it_done_*_v1.csv"
 files = [os.path.basename(x) for x in glob.glob(filename)]
 
 for index, file_ in enumerate(files):
     file_name = file_.split('.')[0]
     name_parts = file_name.split('_')
-    task_name = '_'.join(name_parts[3:-2])
-    md_name = '-'.join(name_parts[3:-2])
+    task_name = '_'.join(name_parts[3:-3])
+    md_name = '-'.join(name_parts[3:-3])
 
     #: Upload prod gid file to S3
     upload_task = S3FileTransferOperator(
