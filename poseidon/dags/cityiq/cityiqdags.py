2bac02bed0a3febe2ef7214eae0db1a909b7f236
Andrell Bower <bowera@gmail.com>
2019-08-26 16:47:32 -0700

    Streamlining cityiq parking in&out

--- poseidon/dags/cityiq/cityiqdags.py
+++ poseidon/dags/cityiq/cityiqdags.py
@@ -1,10 +1,11 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
-from dags.cityiq.cityiqjobs import get_token_response, get_pkout_bbox, get_pkin_bbox #, get_assets, get_asset_details, get_pedevt_bbox, get_tfevt_bbox
+from dags.cityiq.cityiqjobs import *
 from trident.util import general
 from trident.util.notifications import notify
 from trident.operators.s3_file_transfer_operator import S3FileTransferOperator
-from datetime import *
+from airflow.operators.latest_only_operator import LatestOnlyOperator
+from datetime import datetime
 
 
 # All times in Airflow UTC.  Set Start Time in PST?
@@ -18,6 +19,10 @@ dag = DAG(
     start_date=start_date, 
     schedule_interval=general.schedule['cityiq'])
 
+#: Latest Only Operator for cityiq
+cityiq_latest_only = LatestOnlyOperator(
+    task_id='cityiq_latest_only', dag=dag)
+
 get_token_response = PythonOperator(
     task_id = 'get_token_response',
     python_callable=get_token_response,
@@ -26,78 +31,43 @@ get_token_response = PythonOperator(
     on_success_callback=notify,
     dag=dag)
 
-get_pkout_bbox = PythonOperator(
-    task_id='get_pkout_bbox',
+get_parking_bbox = PythonOperator(
+    task_id='get_parking_bbox',
     provide_context=True,
-    python_callable=get_pkout_bbox,
+    python_callable=get_events,
     on_failure_callback=notify,
     on_retry_callback=notify,
     on_success_callback=notify,
     dag=dag)
 
-get_pkin_bbox = PythonOperator(
-    task_id='get_pkin_bbox',
-    provide_context=True,
-    python_callable=get_pkin_bbox,
-    on_failure_callback=notify,
-    on_retry_callback=notify,
-    on_success_callback=notify,
-    dag=dag)
+event_files = ["pkin","pkout"]
 
-# get_pedevt_bbox = PythonOperator(
-#     task_id='get_pedevt_bbox',
-#     provide_context=True,
-#     python_callable=get_pedevt_bbox,
-#     on_failure_callback=notify,
-#     on_retry_callback=notify,
-#     on_success_callback=notify,
-#     dag=dag)
-
-# get_tfevt_bbox = PythonOperator(
-#     task_id='get_tfevt_bbox',
-#     provide_context=True,
-#     python_callable=get_tfevt_bbox,
-#     on_failure_callback=notify,
-#     on_retry_callback=notify,
-#     on_success_callback=notify,
-#     dag=dag)
-
-# get_assets = PythonOperator(
-#     task_id='get_assets',
-#     provide_context=True,
-#     python_callable=get_assets,
-#     on_failure_callback=notify,
-#     on_retry_callback=notify,
-#     on_success_callback=notify,
-#     dag=dag)
-
-upstream_dict = {
-    'pkin': get_pkin_bbox, 
-    'pkout': get_pkout_bbox, 
-    'pedevt': get_pedevt_bbox
-    # 'tfevt': get_tfevt_bbox,
-    # 'assets': get_assets
-    }
-
-for prefix in upstream_dict.keys():
+for file in event_files:
     file_time = datetime.now().strftime('%Y_%m_%d_') 
-    file_name = file_time + prefix + '.json'
+    file_name = f'{file_time}_{file}.json'
     s3_upload = S3FileTransferOperator( # creating a different upload object for each...
-        task_id='upload_' + prefix,
-        source_base_path='/data/temp',
+        task_id=f'upload_{file}',
+        source_base_path=conf['prod_data_dir'],
         source_key=file_name,
         dest_s3_conn_id=conf['default_s3_conn_id'],
         dest_s3_bucket=conf['dest_s3_bucket'],
-        dest_s3_key='cityiq/' + prefix + '/' + file_name,
+        dest_s3_key=f'cityiq/{file_name}',
         on_failure_callback=notify,
         on_retry_callback=notify,
         on_success_callback=notify,
         replace=True,
         dag=dag)
+
+    #: Upload after getting events
+    s3_upload.set_upstream(get_parking_bbox)
     
-    # Set all upstream of get_token_response and downstream of s3_upload
-    upstream_dict[prefix].set_upstream(get_token_response)
-    s3_upload.set_upstream(upstream_dict[prefix])
+
+#: Execution Rules
+
+#: Must get token after latest only operator
+get_token_response.set_upstream(cityiq_latest_only)
+#: Get events after getting token
+get_parking_bbox.set_upstream(get_token_response)
 
 
     
--- poseidon/dags/cityiq/cityiqjobs.py
+++ poseidon/dags/cityiq/cityiqjobs.py
@@ -1,14 +1,22 @@
+"""CityIQ _jobs file."""
 import requests
 import json
 from datetime import *
+import logging
+from trident.util import general
 
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from trident.operators.s3_file_transfer_operator import S3FileTransferOperator
 
-# starting to test the dag functuon
+conf = general.config
+bbox = "33.077762:-117.663817,32.559574:-116.584410"
+event_types = ["PKIN","PKOUT"]
+token_url = "https://auth.aa.cityiq.io/oauth/token"
+event_url = "https://sandiego.cityiq.io/api/v2/event/locations/events"
+
 def get_token_response():
-	url = "https://auth.aa.cityiq.io/oauth/token"
+	""" Get auth token for making requests """
 
 	query_string = {"grant_type":"client_credentials"}
 
@@ -18,225 +26,77 @@ def get_token_response():
 		'User-Agent': "PostmanRuntime/7.11.0",
 		'Accept': "*/*",
 		'Cache-Control': "no-cache",
-		'Postman-Token': "c9257e3c-29d1-4999-912b-a488c04d397e,d9e0d977-bcd1-4ff3-a10a-8abc960f7c7d",
 		'Host': "auth.aa.cityiq.io",
 		'accept-encoding': "gzip, deflate",
 		'Connection': "keep-alive",
 		'cache-control': "no-cache"
 		}
 
-	response = requests.request("GET", url, data=payload, headers=headers, params=query_string)
-	token_data = json.loads(response.text)
-	token = token_data['access_token']
-	print(token)
-	return token
+	try:
+
+		response = requests.request("GET", token_url, data=payload, headers=headers, params=query_string)
+		token_data = json.loads(response.text)
+		token = token_data['access_token']
+		logging.info(token)
+		return token
+
+	except Exception as e:
+
+		logging.error(e)
 
 
-def get_pkout_bbox(**kwargs):
-	ti = kwargs['ti']
-	token = ti.xcom_pull(task_ids='get_token_response') # get outputs from get_token_response task
+def get_events(**kwargs):
+	""" Send GET request for events using token """
 	
-	url = "https://sandiego.cityiq.io/api/v2/event/locations/events"
-	end = datetime.now()
-	start = end - timedelta(days=1)
-	query_string = {
-		"bbox": "33.077762:-117.663817,32.559574:-116.584410",
-		"locationType": "PARKING_ZONE",
-		"eventType": "PKOUT",
-		"startTime": str(int(start.timestamp() * 1000)),
-		"endTime": str(int(end.timestamp() * 1000)),
-		"pageSize": "100"
-		}
-	headers = {
-		'Authorization': "Bearer {}".format(token),
-		'predix-zone-id': "SD-IE-PARKING",
-		'cache-control': "no-cache",
-		'postman-token': "e8199816-04a2-29e4-ec0d-1a68a94fe771"
-		}
-	response = requests.request("GET", url, headers=headers, params=query_string)
-	assets = response.json()
-
-	path = '/data/temp/'
-	file_name = datetime.now().strftime('%Y_%m_%d_pkout.json')
-	path += file_name
-	with open(path, 'w') as f:
-		json.dump(assets, f)
-	return
-
-
-def get_pkin_bbox(**kwargs):
+	logging.info("Getting token from previous task")
 	ti = kwargs['ti']
 	token = ti.xcom_pull(task_ids='get_token_response')
-
-	url = "https://sandiego.cityiq.io/api/v2/event/locations/events"
+	
+	logging.info("Calculating start and end dates")
 	end = datetime.now()
 	start = end - timedelta(days=1)
-	query_string = {
-		"bbox": "33.077762:-117.663817,32.559574:-116.584410",
-		"locationType": "PARKING_ZONE",
-		"eventType": "PKIN",
-		"startTime": str(int(start.timestamp() * 1000)),
-		"endTime": str(int(end.timestamp() * 1000)),
-		"pageSize": "100"
-		}
+
+	logging.info(f"Starting at {start} and ending at {end}")
+
 	headers = {
-		'Authorization': "Bearer {}".format(token),
-		'predix-zone-id': "SD-IE-PARKING",
-		'cache-control': "no-cache",
-		'postman-token': "63188df1-b0db-c553-aecd-4882b65e5dcc"
+	'Authorization': "Bearer {}".format(token),
+	'predix-zone-id': "SD-IE-PARKING",
+	'cache-control': "no-cache"
 	}
 
-	response = requests.request("GET", url, headers=headers, params=query_string)
-	assets = response.json()
+	for et in event_types:
+		logging.info(f"Retrieving events for {et}")
 
-	path = '/data/temp/'
-	file_name = datetime.now().strftime('%Y_%m_%d_pkin.json')
-	path += file_name
-	with open(path, 'w') as f:
-		json.dump(assets, f)
-	return
+		query_string = {
+			"bbox": bbox,
+			"locationType": "PARKING_ZONE",
+			"eventType": et,
+			"startTime": str(int(start.timestamp() * 1000)),
+			"endTime": str(int(end.timestamp() * 1000)),
+			"pageSize": "100"
+		}
 
-# def get_pedevt_bbox(**kwargs):
-# 	ti = kwargs['ti']
-# 	token = ti.xcom_pull(task_ids='get_token_response')
+		try:
+			response = requests.request("GET", event_url, headers=headers, params=query_string)
 
-# 	url = "https://sandiego.cityiq.io/api/v2/event/locations/events"
-# 	end = datetime.now()
-# 	start = end - timedelta(days=1)
-# 	query_string = {
-# 		"bbox": "33.077762:-117.663817,32.559574:-116.584410",
-# 		"locationType": "WALKWAY",
-# 		"eventType": "PEDEVT",
-# 		"startTime": str(int(start.timestamp() * 1000)),
-# 		"endTime": str(int(end.timestamp() * 1000)),
-# 		"pageSize": "100"
-# 		}
-# 	headers = {
-# 		'Authorization': "Bearer {}".format(token),
-# 		'predix-zone-id': "SD-IE-PEDESTRIAN",
-# 		'cache-control': "no-cache",
-# 		'postman-token': "5253f845-d1c8-4dd4-bea6-65a192f8d9e8"
-# 	}
+			if response.status_code != 200:
+				
+				raise Exception(response.status_code)
+			
+			else:
+				logging.info("Got events")
+				assets = response.json()
+				file_date = datetime.now().strftime('%Y_%m_%d')
+				file_path = f"{conf['prod_data_dir']}/{file_date}_{et.lower()}.json"
+				logging.info("Writing events")
+				with open(file_path, 'w') as f:
+					json.dump(assets, f)
 
-# 	response = requests.request("GET", url, headers=headers, params=query_string)
-# 	assets = response.json()
+				logging.info(f"Successfully requested events for {et}")
 
-# 	path = '/data/temp/'
-# 	file_name = datetime.now().strftime('%Y_%m_%d_pedevt.json')
-# 	path += file_name
-# 	with open(path, 'w') as f:
-# 		json.dump(assets, f)
-# 	return
+		except Exception as e:
 
-
-# def get_tfevt_bbox(**kwargs):
-# 	ti = kwargs['ti']
-# 	token = ti.xcom_pull(task_ids='get_token_response')
-
-# 	url = "https://sandiego.cityiq.io/api/v2/event/locations/events"
-# 	end = datetime.now()
-# 	start = end - timedelta(days=1)
-# 	query_string = {
-# 		"bbox": "33.077762:-117.663817,32.559574:-116.584410",
-# 		"locationType": "WALKWAY",
-# 		"eventType": "PEDEVT",
-# 		"startTime": str(int(start.timestamp() * 1000)),
-# 		"endTime": str(int(end.timestamp() * 1000)),
-# 		"pageSize": "100"
-# 		}
-# 	headers = {
-# 		'Authorization': "Bearer {}".format(token),
-# 		'predix-zone-id': "SD-IE-TRAFFIC",
-# 		'User-Agent': "PostmanRuntime/7.15.0",
-# 		'Accept': "*/*",
-# 		'Cache-Control': "no-cache",
-# 		'Postman-Token': "ef36dfb8-dde2-47c2-a65e-b6b35d8397c2,7f7cb66d-f7e7-4f2f-b841-fbe580ea4287",
-# 		'Host': "sandiego.cityiq.io",
-# 		'accept-encoding': "gzip, deflate",
-# 		'Connection': "keep-alive",
-# 		'cache-control': "no-cache"
-#  	}
-
-# 	response = requests.request("GET", url, headers=headers, params=query_string)
-# 	assets = response.json()
-
-# 	path = '/data/temp/'
-# 	file_name = datetime.now().strftime('%Y_%m_%d_tfevt.json')
-# 	path += file_name
-# 	with open(path, 'w') as f:
-# 		json.dump(assets, f)
-# 	return
-
-
-# def get_assets(**kwargs):
-# 	ti = kwargs['ti']
-# 	token = ti.xcom_pull(task_ids='get_token_response') # get outputs from get_token_response task
-
-# 	url = "https://sandiego.cityiq.io/api/v2/metadata/assets/search"
-# 	query_string = {
-# 		"bbox": "33.077762:-117.663817,32.559574:-116.584410",
-# 		"page": "0",
-# 		"size": "2000000",
-# 		"q": "assetType:CAMERA"
-# 		}
-# 	payload = ""
-# 	headers = {
-# 		'Authorization': "Bearer {}".format(token),
-# 		'Predix-Zone-Id': "SD-IE-TRAFFIC",
-# 		#'User-Agent': "PostmanRuntime/7.11.0",
-# 		'Accept': "*/*",
-# 		'Cache-Control': "no-cache",
-# 		#'Postman-Token': "a90832bf-90a6-4e4c-a5b6-4c3414628a00,9405e923-b326-4760-8d29-9222b0d919ec",
-# 		'Host': "sandiego.cityiq.io",
-# 		#'accept-encoding': "gzip, deflate",
-# 		'Connection': "keep-alive",
-# 		'cache-control': "no-cache"
-# 		}
-
-# 	response = requests.request("GET", url, data=payload, headers=headers, params=query_string)
-# 	assets = response.json()
-
-# 	### --- Modify file path --- ###
-# 	path = '/data/temp/'
-# 	file_name = datetime.now().strftime('%Y_%m_%d_assets.json')
-# 	path += file_name
-# 	with open(path, 'w') as f:
-# 		json.dump(assets, f)
-# 	return
-
-
-# def get_asset_details():
-# 	with open('/usr/local/airflow/poseidon/dags/cityiq/assets.json') as f:
-# 		temp = json.loads(f.read())
-# 		data = temp.get('content')
-# 		d=0
-# 		assets = [] 
-# 		while d < len(data):
-# 			assets.append(data[d].get('assetUid'))
-# 			d+=1
-# 		print(assets)
-    
-# 	print('getting asset details')
-# 	#data = json.loads(temp)
-# 	#data[0]
-# 	#print([d.get('assetUid') for d in data])
-
-
-# def get_parking(assetID):
-# 	token = get_token_response()
-# 	url = "https://sandiego.cityiq.io/api/v2/event/assets/{}/events".format(assetID)
-# 	#look into increasing pageSize
-# 	query_string = {
-# 		"eventType": "PKIN",
-# 		"startTime": "1558049253306",
-# 		"endTime": "1558567653306",
-# 		"pageSize": "100"
-# 		}
-# 	headers = {
-# 		'Authorization': "Bearer {}".format(token),
-# 		'predix-zone-id': "SD-IE-PARKING",
-# 		'cache-control': "no-cache",
-# 		'postman-token': "abbbec83-ff55-1748-f703-b617bd17b794"
-# 		}
-# 	response = requests.request("GET", url, headers=headers, params=query_string)
-# 	return
+			logging.error(e)
+		
+	
+	return "Successfully requested events for all event types"
