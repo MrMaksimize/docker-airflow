7014ed76aa60f1b792d3d168b607e5acd30c2c5f
Andrell Bower <bowera@gmail.com>
2019-10-30 21:28:41 -0700

    Adding a new schedule for some documentum tables

--- poseidon/dags/city_docs/documentum_15_dags.py
+++ poseidon/dags/city_docs/documentum_15_dags.py
@@ -1,4 +1,4 @@
-""" Documentum web tables with one hour schedule update _dags file"""
+""" Documentum web tables with daily schedule update _dags file"""
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from trident.operators.s3_file_transfer_operator import S3FileTransferOperator
@@ -10,20 +10,21 @@ from trident.util.notifications import notify
 import dags.city_docs.documentum_name as dn
 
 from dags.city_docs.city_docs_jobs import *
-from datetime import datetime
 
 # All times in Airflow UTC.  Set Start Time in PST?
+
 args = general.args
 conf = general.config
-schedule = general.schedule['documentum_hr_30']
-start_date = general.start_date['documentum_hr_30']
+schedule = general.schedule['documentum_hr_15']
+start_date = general.start_date['documentum_hr_15']
+prod_data = conf['prod_data_dir']
+schedule_mode = 'schedule_hourly_15'
+
 
 #: Dag spec
-dag = DAG(dag_id='documentum_hourly_30', catchup=False, default_args=args, start_date=start_date, schedule_interval=schedule)
-prod_data = conf['prod_data_dir']
-schedule_mode = 'schedule_hourly_30'
+dag = DAG(dag_id='documentum_hourly_15', catchup=False, default_args=args, start_date=start_date, schedule_interval=schedule)
 
-documentum_docs_latest_only = LatestOnlyOperator(task_id='documentum_others_docs_latest_only', dag=dag)
+#documentum_docs_latest_only = LatestOnlyOperator(task_id='documentum_24_docs_latest_only', dag=dag)
 
 #: Get documentum tables
 get_doc_tables = PythonOperator(
@@ -37,32 +38,17 @@ get_doc_tables = PythonOperator(
 
 div_doc_table = PythonOperator(
     task_id='divide_doc_table',
-    python_callable=latest_res_ords,
+    python_callable=split_reso_ords,
     on_failure_callback=notify,
     on_retry_callback=notify,
     on_success_callback=notify,
-    dag=dag)
-
-upload_reso_ord = S3FileTransferOperator(
-    task_id='upload_documentum_reso_ordinance_latest',
-    source_base_path=conf['prod_data_dir'],
-    source_key='documentum_scs_council_reso_ordinance_v_2016_current.csv',
-    dest_s3_conn_id=conf['default_s3_conn_id'],
-    dest_s3_bucket=conf['dest_s3_bucket'],
-    dest_s3_key='city_docs/documentum_scs_council_reso_ordinance_v_2016_current.csv',
-    on_failure_callback=notify,
-    on_retry_callback=notify,
-    on_success_callback=notify,
-    replace=True,
     dag=dag)
 
 #: Execution rules
 #: documentum_docs_latest_only must run before get_doc_tables
-get_doc_tables.set_upstream(documentum_docs_latest_only)
+#get_doc_tables.set_upstream(documentum_docs_latest_only)
 #: get_doc_tables must run before div_doc_table
 div_doc_table.set_upstream(get_doc_tables)
-#: get_doc_tables must run before upload_doc_tables
-upload_reso_ord.set_upstream(div_doc_table)
 
 files = [f for f in os.listdir(conf['prod_data_dir'])]
 tables_other = dn.table_name(schedule_mode)
@@ -71,7 +57,7 @@ for f in files:
     name_parts = file_name.split('_')
     if name_parts[0] == "documentum":
         file_check = '_'.join(name_parts[1:]).upper()
-        if file_check in tables_other:
+        if file_check in tables_other or ("RESO_ORDINANCE" in file_check and not "2016" in file_check):
             #: Upload onbase prod files to S3
             upload_doc_tables = S3FileTransferOperator(
                 task_id='upload_' + file_name,
