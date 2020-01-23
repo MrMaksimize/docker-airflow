e54301844e9f28af07cc54699c4ae7266fb122cb
Andrell Bower <bowera@gmail.com>
2020-01-23 11:05:06 -0800

    updating method to accommodate backfilling

--- poseidon/dags/pd/pd_cfs_dags.py
+++ poseidon/dags/pd/pd_cfs_dags.py
@@ -8,8 +8,8 @@ from datetime import datetime, timedelta
 from dags.pd.pd_cfs_jobs import *
 from trident.util import general
 from trident.util.notifications import notify
-from trident.util.seaboard_updates import update_seaboard_date, get_seaboard_update_dag
-
+from trident.util.seaboard_updates import update_seaboard_date, get_seaboard_update_dag, update_json_date
+import os
 
 args = general.args
 conf = general.config
@@ -20,15 +20,11 @@ dag = DAG(
     dag_id='pd_cfs', default_args=args, start_date=start_date, schedule_interval=schedule['pd_cfs'])
 
 
-#: Latest Only Operator for pd_cfs
-pd_cfs_latest_only = LatestOnlyOperator(
-    task_id='pd_cfs_latest_only', dag=dag)
-
-
 #: Get CFS data from FTP and save to temp folder
-get_cfs_data = BashOperator(
+get_cfs_data = PythonOperator(
     task_id='get_cfs_data',
-    bash_command=get_cfs_data(),
+    python_callable=get_cfs_data,
+    provide_context=True,
     on_failure_callback=notify,
     on_retry_callback=notify,
     on_success_callback=notify,
@@ -38,6 +34,7 @@ get_cfs_data = BashOperator(
 process_cfs_data = PythonOperator(
     task_id='process_cfs_data',
     python_callable=process_cfs_data,
+    provide_context=True,
     on_failure_callback=notify,
     on_retry_callback=notify,
     on_success_callback=notify,
@@ -47,10 +44,21 @@ process_cfs_data = PythonOperator(
 cfs_to_S3 = S3FileTransferOperator(
     task_id='cfs_to_S3',
     source_base_path=conf['prod_data_dir'],
-    source_key='pd_calls_for_service_'+curr_year+'_datasd.csv',
+    source_key='pd_calls_for_service_2020_datasd.csv',
     dest_s3_bucket=conf['dest_s3_bucket'],
     dest_s3_conn_id=conf['default_s3_conn_id'],
-    dest_s3_key='pd/pd_calls_for_service_'+curr_year+'_datasd.csv',
+    dest_s3_key='pd/pd_calls_for_service_2020_datasd.csv',
+    on_failure_callback=notify,
+    on_retry_callback=notify,
+    on_success_callback=notify,
+    dag=dag)
+
+#: Update data inventory json
+update_json_date = PythonOperator(
+    task_id='update_json_date',
+    python_callable=update_json_date,
+    provide_context=True,
+    op_kwargs={'ds_fname': 'police_calls_for_service'},
     on_failure_callback=notify,
     on_retry_callback=notify,
     on_success_callback=notify,
@@ -61,14 +69,4 @@ update_pd_cfs_md = get_seaboard_update_dag('police-calls-for-service.md', dag)
 
 #: Execution rules:
 
-#: pd_cfs_latest_only must run before pd_cfs_data
-get_cfs_data.set_upstream(pd_cfs_latest_only)
-
-#: Data processing is triggered after data retrieval.
-process_cfs_data.set_upstream(get_cfs_data)
-
-#: Data upload to S3 is triggered after data processing completion.
-cfs_to_S3.set_upstream(process_cfs_data)
-
-#: Github update depends on S3 upload success.
-update_pd_cfs_md.set_upstream(cfs_to_S3)
+get_cfs_data >> process_cfs_data >> cfs_to_S3 >> [update_pd_cfs_md,update_json_date]
--- poseidon/dags/pd/pd_cfs_jobs.py
+++ poseidon/dags/pd/pd_cfs_jobs.py
@@ -1,51 +1,68 @@
 """PD calls_for_service _jobs file."""
-import string
-import os
 import logging
-import glob
+import os
 import pandas as pd
-import numpy as np
-from datetime import datetime
+import pendulum
+import subprocess
+import glob
+from subprocess import Popen, PIPE
 from trident.util import general
 
 conf = general.config
-curr_year = datetime.now().strftime('%Y')
 
-
-def get_cfs_data():
+def get_cfs_data(**context):
     """Download daily raw cfs data from FTP."""
-    wget_str = "wget -np --continue " \
-        + "--user=$ftp_user " \
-        + "--password='$ftp_pass' " \
-        + "--directory-prefix=$temp_dir " \
-        + "ftp://ftp.datasd.org/uploads/sdpd/" \
-        + "calls_for_service/calls_for_service_*_*_{0}.csv".format(curr_year)
 
-    tmpl = string.Template(wget_str)
-    command = tmpl.substitute(
-        ftp_user=conf['ftp_datasd_user'],
-        ftp_pass=conf['ftp_datasd_pass'],
-        temp_dir=conf['temp_data_dir']
-    )
+    exec_date = context['execution_date']
+    # Exec date returns a Pendulum object
+    # Running this job at 5p should capture files for day of exec
 
-    return command
+    # File name does not have zero-padded numbers
+    # But month is spelled, abbreviated
+    # Pattern is month_day_year
+    filename = f"{exec_date.strftime('%b')}_" \
+    f"{exec_date.day}_" \
+    f"{exec_date.year}"
+
+    fpath = f"calls_for_service_{filename}.csv"
+
+    logging.info(f"Checking FTP for {filename}")
+
+    command = f"cd {conf['temp_data_dir']} && " \
+    f"curl --user {conf['ftp_datasd_user']}:{conf['ftp_datasd_pass']} " \
+    f"-o {fpath} " \
+    f"ftp://ftp.datasd.org/uploads/sdpd/calls_for_service/" \
+    f"{fpath} -sk"
+
+    logging.info(command)
+
+    p = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+    output, error = p.communicate()
+    
+    if p.returncode != 0:
+        raise Exception(p.returncode)
+    else:
+        logging.info("Found file")
+        # return filename as context for next task
+        return filename
 
 
-def process_cfs_data():
+def process_cfs_data(**context):
     """Update production data with new data."""
-    logging.info('Combining daily CFS files.')
-    path = conf['temp_data_dir']
-    allFiles = glob.glob(os.path.join(path, f"calls_for_service_*_*_{curr_year}.csv"))
-    np_array_list = []
-    for file_ in allFiles:
-        df = pd.read_csv(file_, header=None, error_bad_lines=False, low_memory=False)
-        np_array_list.append(df.as_matrix())
-        
-    comb_np_array = np.vstack(np_array_list)
 
-    temp_frame = pd.DataFrame(comb_np_array)
+    logging.info("Reading new cfs file")
+    filename = context['task_instance'].xcom_pull(task_ids='get_cfs_data')
+    fpath = f"calls_for_service_{filename}.csv"
+
+    logging.info(f"Looking for {fpath}")
+
+    temp_frame = pd.read_csv(f"{conf['temp_data_dir']}/{fpath}",
+        header=None,
+        error_bad_lines=False,
+        low_memory=False)
 
     dtypes = {'address_number_primary':str,
+    'call_type':str,
     'beat':str,
     'priority':str,
     'day_of_week':str
@@ -53,31 +70,75 @@ def process_cfs_data():
 
     logging.info('Adding recent data to CFS production file.')
     curr_frame = pd.read_csv(
-        f"{conf['prod_data_dir']}/pd_calls_for_service_{curr_year}_datasd.csv",
-        parse_dates=['date_time'],dtype=dtypes)
+        f"{conf['prod_data_dir']}/pd_calls_for_service.csv",
+        parse_dates=['date_time'],
+        dtype=dtypes,
+        low_memory=False
+        )
 
-    columns_names = curr_frame.columns.values
-    temp_frame.columns = columns_names
+    col_names = curr_frame.columns.values
+    temp_frame.columns = col_names
 
-    temp_frame['date_time'] = pd.to_datetime(temp_frame['date_time'],errors='coerce')
+    logging.info('Fixing dtypes')
+
+    temp_frame.loc[:,'date_time'] = pd.to_datetime(temp_frame.loc[:,'date_time'],
+        errors='coerce')
 
     for str_col in dtypes.keys():
-        temp_frame[str_col] = temp_frame[str_col].astype(str)
+        temp_frame.loc[:,str_col] = temp_frame.loc[:,str_col].astype(str)
+
+    logging.info('Appending new records to existing file')
 
     prod_frame = curr_frame.append(temp_frame, ignore_index=True)
+    logging.info(f'New file has {prod_frame.shape[0]} records')
     prod_frame = prod_frame.drop_duplicates(subset=['incident_num'])
+    logging.info(f'After dedupe, file has {prod_frame.shape[0]} records')
     prod_frame = prod_frame.sort_values(by='date_time', ascending=True)
 
-    logging.info('Exporting updated CFS production data to csv.')
+    logging.info('Exporting full data to csv')
 
-    prod_file = conf['prod_data_dir'] \
-        + '/pd_calls_for_service_' \
-        + curr_year \
-        + '_datasd.csv'
-
+    prod_file = f"{conf['prod_data_dir']}/pd_calls_for_service.csv"
+    
     general.pos_write_csv(
         prod_frame,
         prod_file,
         date_format=conf['date_format_ymd_hms'])
+
+    logging.info('Splitting off CY files')
+
+    min_yr = temp_frame['date_time'].min().year
+    max_yr = temp_frame['date_time'].max().year
+
+    if min_yr != max_yr:
+
+        for i in range(min_yr,max_yr+1):
+            
+            logging.info(f'Writing new file for {i}')
+            
+            yr_subset = prod_frame[(prod_frame['date_time'] >= f'01-01-{i} 00:00:00') &
+                (prod_frame['date_time'] < f'01-01-{i+1} 00:00:00')]
+
+            yr_file = f"{conf['prod_data_dir']}/pd_calls_for_service_{i}_datasd.csv"
+
+            general.pos_write_csv(
+                yr_subset,
+                yr_file,
+                date_format=conf['date_format_ymd_hms'])
+
+    else:
+
+        yr_file = f"{conf['prod_data_dir']}/pd_calls_for_service_{max_yr}_datasd.csv"
+
+        general.pos_write_csv(
+            yr_subset,
+            yr_file,
+            date_format=conf['date_format_ymd_hms'])
+
+
+
+    os_path = conf['prod_data_dir'] + "/pd_calls_for_service_*.csv"
+    files = [os.path.basename(x) for x in glob.glob(filename)]
+    for file in files:
+        logging.info(os.path.getmtime(file))
 
     return 'Successfully processed CFS data.'
