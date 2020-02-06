cf1c8944cb2990d234239b9dfb946476f160172b
Bryan Olson <bryanolson@bryans-mbp.ad.sannet.gov>
2020-02-06 14:32:35 -0800

    removed power.py adding helper functions

--- poseidon/dags/pv_production/pv_prod_jobs.py
+++ poseidon/dags/pv_production/pv_prod_jobs.py
@@ -119,3 +119,61 @@ def update_pv_prod(**context):
 	return f"Successfully wrote prod file with {results} records"
 
 #Push to Lucid
+
+#Helper Functions
+
+#TODO (THIS HAS NOT BEEN TESTED)
+def API_call(start_date, end_date, elem_paths, attr, two_hours=False, resolution="raw", fp=None):
+	    baseurl = 'https://api.powerfactorscorp.com' 
+        headers = {"Ocp-Apim-Subscription-Key": PRIMARY_KEY}
+
+        dataURL = baseurl + '/drive/v2/data'
+        results = {path: [] for path in elem_paths} # To store values for each element
+        dates = [(start_date, end_date)]
+
+        num_vals = 0
+        start_tstamp = None
+        end_tstamp = None
+        for path in elem_paths: # Loop through each element in list
+            for start, end in dates: # Iterate through list of start and end times
+                body = {"startTime": start,
+                        "endTime": end,
+                        "resolution": resolution,
+                        "attributes": attr,
+                        "ids": path}
+                r = requests.post(dataURL, headers=headers, data=body).json() # Use POST to avoid hitting max URL length w/ many params
+                results[path] += r['assets'][0]['attributes'][0]['values'][1:] # Append readings for this time period to list of readings for this element
+                if start_tstamp is None: # Get the start timestamp of the entire time period
+                    start_tstamp = pd.to_datetime(r['assets'][0]['startTime'][:19])+dt.timedelta(minutes=5)  
+                
+            end_tstamp = pd.to_datetime(r['assets'][0]['endTime'][:19])
+            num_vals = len(results[path])
+       
+        tstamps = pd.date_range(start_tstamp, end_tstamp, periods=num_vals)
+        df_5min = pd.DataFrame(index=tstamps, data=results)
+        df_15min = df_5min.resample('15T', label='right', closed='right').mean()
+        
+        if fp:
+            df_15min.to_csv(fp)
+            return
+        else:
+            return df_15min, df_5min
+
+#TO DO (THIS NEEDS TO BE REWRITTEN)
+def API_to_csv():
+	startDate = (datetime.datetime.now() - datetime.timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:00")
+	endDate = (datetime.datetime.now() - datetime.timedelta(minutes=20)).strftime("%Y-%m-%d %H:%M:00")
+	startDate = general.utc_to_pst(startDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
+	endDate = general.utc_to_pst(endDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
+
+	elem_paths = ['2000.05.088.SWG01.MTR01']
+
+	print("CALLING API WITH {} {} {} {}".format(startDate, endDate, elem_paths, attr))
+
+	df_5min, df_15min = p.get_data(startDate, endDate, elem_paths, attr, True)
+
+	df_5min = df_5min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
+	df_15min = df_15min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
+	df_15min.index.name = 'Timestamp'
+
+	general.pos_write_csv(df_15min, hourly_file, index=True, date_format=conf['date_format_ymd_hms'])
