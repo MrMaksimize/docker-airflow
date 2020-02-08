86e4ac5215e192738f2d23bbc70a6d56750f712b
Bryan Olson <bryanolson@bryans-mbp.ad.sannet.gov>
2020-02-07 16:23:57 -0800

    not working but updated helper functions

--- poseidon/dags/pv_production/pv_prod_jobs.py
+++ poseidon/dags/pv_production/pv_prod_jobs.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import logging
 
-from trident.util import general, power
+from trident.util import general
 
 conf = general.config
 
@@ -26,7 +26,7 @@ def asdf():
 #API Call
 def get_pv_data_write_temp(**context):
 	# 2000.05.088 = Sierra/Kearney Mesa Library, 2000.05.073 = Malcolm X Library, 2000.06.006 = Pt. Loma Library
-	p = power.Power(PRIMARY_KEY)
+	
 
 	pv_sites = {'2000.05.066.SWG01.MTR01': 'Carmel Valley Rec Center', 
 					'2000.05.088.SWG01.MTR01': 'Serra Mesa-Kearny Mesa Library', 
@@ -44,7 +44,7 @@ def get_pv_data_write_temp(**context):
 					'2000.06.047.SWG01.MTR01': 'Balboa Park Inspiration Point', 
 					'2000.06.053.SWG01.MTR01': 'Park De La Cruz Rec Center'}
 
-	attr = 'AC_POWER'
+
 
 	currHour = datetime.datetime.now()
 	currHour = currHour.hour
@@ -74,7 +74,7 @@ def get_pv_data_write_temp(**context):
 		startDate = general.utc_to_pst(startDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
 		endDate = general.utc_to_pst(endDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
 
-		df_5min, df_15min = p.get_data(startDate, endDate, elem_paths, attr, True)
+		df_5min, df_15min = get_data(startDate, endDate, elem_paths, attr, True)
 
 		df_5min = df_5min.rename(columns=pv_sites)
 		df_15min = df_15min.rename(columns=pv_sites)
@@ -91,7 +91,7 @@ def get_pv_data_write_temp(**context):
 
 	print("CALLING API WITH {} {} {} {}".format(startDate, endDate, elem_paths, attr))
 
-	df_5min, df_15min = p.get_data(startDate, endDate, elem_paths, attr, True)
+	df_5min, df_15min = get_data(startDate, endDate, elem_paths, attr, True)
 
 	df_5min = df_5min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
 	df_15min = df_15min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
@@ -126,9 +126,8 @@ def update_pv_prod(**context):
 #Push to Lucid
 
 #Helper Functions
-'''
 #TODO (THIS HAS NOT BEEN TESTED)
-def API_call(start_date, end_date, elem_paths, attr, two_hours=False, resolution="raw", fp=None):
+def get_data(start_date, end_date, elem_paths, attr, two_hours=False, resolution="raw", fp=None):
 	    baseurl = 'https://api.powerfactorscorp.com' 
         headers = {"Ocp-Apim-Subscription-Key": PRIMARY_KEY}
 
@@ -165,21 +164,45 @@ def API_call(start_date, end_date, elem_paths, attr, two_hours=False, resolution
             return df_15min, df_5min
 
 #TO DO (THIS NEEDS TO BE REWRITTEN)
-def API_to_csv():
+def API_to_csv(elem_paths, interval):
+	if interval == 'hourly':
+		temp_file = conf['temp_data_dir'] + '/pv_hourly_results.csv'
+		prod_file = conf['prod_data_dir'] + '/pv_hourly_production.csv'
+		rename_cols = {'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'}
+	elif interval = 'daily':
+		temp_file = conf['temp_data_dir'] + '/pv_daily_results.csv'
+		prod_file = conf['prod_data_dir'] + '/pv_production.csv'
+		rename_cols = {'2000.05.066.SWG01.MTR01': 'Carmel Valley Rec Center', 
+					'2000.05.088.SWG01.MTR01': 'Serra Mesa-Kearny Mesa Library', 
+					'2000.05.100.SWG01.MTR01': 'Fire Repair Facility', 
+					'2000.05.114.SWG01.MTR01': 'Rancho Bernardo Senior Center', 
+					'2000.05.117.SWG01.MTR01': 'Police Station Eastern Division', 
+					'2000.05.120.SWG01.MTR01': 'Police Station Southern Division', 
+					'2000.06.005.SWG01.MTR01': 'Mountain View Rec Center', 
+					'2000.06.007.SWG01.MTR01': 'Police Station Northern Division', 
+					'2000.06.010.SWG01.MTR01': 'Tierrasanta Rec Center & Pool', 
+					'2000.06.021.SWG01.MTR01': 'Police Station Western Division ', 
+					'2000.06.027.SWG01.MTR01': 'Mission Valley Library', 
+					'2000.06.029.SWG01.MTR01': 'Police Station Central Division', 
+					'2000.06.046.SWG01.MTR01': 'Mission Trails Regional Park', 
+					'2000.06.047.SWG01.MTR01': 'Balboa Park Inspiration Point', 
+					'2000.06.053.SWG01.MTR01': 'Park De La Cruz Rec Center'}
+	
+	attr = 'AC_POWER'
+
 	startDate = (datetime.datetime.now() - datetime.timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:00")
 	endDate = (datetime.datetime.now() - datetime.timedelta(minutes=20)).strftime("%Y-%m-%d %H:%M:00")
+
 	startDate = general.utc_to_pst(startDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
 	endDate = general.utc_to_pst(endDate,"%Y-%m-%d %H:%M:00","%Y-%m-%d %H:%M:00")
 
-	elem_paths = ['2000.05.088.SWG01.MTR01']
 
 	print("CALLING API WITH {} {} {} {}".format(startDate, endDate, elem_paths, attr))
 
-	df_5min, df_15min = p.get_data(startDate, endDate, elem_paths, attr, True)
+	df_5min, df_15min = get_data(startDate, endDate, elem_paths, attr, True)
 
-	df_5min = df_5min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
-	df_15min = df_15min.rename(columns={'2000.05.088.SWG01.MTR01':'Serra Mesa-Kearny Mesa Library'})
+	df_5min = df_5min.rename(columns=rename_cols)
+	df_15min = df_15min.rename(columns=rename_cols)
 	df_15min.index.name = 'Timestamp'
 
-	general.pos_write_csv(df_15min, hourly_file, index=True, date_format=conf['date_format_ymd_hms'])
-'''
+	general.pos_write_csv(df_15min, temp_file, index=True, date_format=conf['date_format_ymd_hms'])
