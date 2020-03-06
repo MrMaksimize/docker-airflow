1f900c067cdb0fc99187766804e902f6d71a9bcd
Bryan Olson <bryanolson@bryans-mbp.ad.sannet.gov>
2020-03-06 15:31:10 -0800

    Need to test build_prod_file. Not enough memory on local

--- poseidon/dags/parking_meters/parking_meters_jobs.py
+++ poseidon/dags/parking_meters/parking_meters_jobs.py
@@ -55,14 +55,18 @@ def ftp_download_wget(**context):
     for item in after_files:
         if wget_str[122:-5] == item[:-8]:
             dupe_file = item
+        else:
+            dupe_file = 'No Duplicate File'
 
     #Handle returns from wget (error codes, file already exists, no files downloaded)
     if p.returncode != 0:
         raise AirflowException(p.returncode)
     elif len(new_files) == 0 and dupe_file != None:        
         logging.info("Found duplicate file, /data/temp contains : {}".format(dupe_file))
+        raise AirflowException()
     elif len(new_files) == 0:
         logging.info("Found {} *new* file(s) named {}".format(len(new_files), wget_str[122:]))
+        raise AirflowException()
     else:
         logging.info("Found {} file(s)".format(len(new_files)))
         return new_files
@@ -73,15 +77,11 @@ def build_prod_file(**context):
     #Get list of downloaded files from ftp server
     files = context['task_instance'].xcom_pull(task_ids='get_parking_files')
 
-    # Look for files for the past week
-    # Job dies sometimes with more files
-    #last_week = (datetime.now() - timedelta(days=6)).day
-    #last_week = (context['execution_date'] - timedelta(days=76)).day
+    list_ = []
 
-    
     for file_ in files:
         try:
-            df = pd.read_csv(file_, index_col=False, header=0)
+            df = pd.read_csv((conf['temp_data_dir'] + '/' + file_), index_col=False, header=0)
             list_.append(df)
             logging.info("Read "+file_)
         except:
@@ -89,35 +89,6 @@ def build_prod_file(**context):
 
     frame = pd.DataFrame()
     frame = pd.concat(list_, ignore_index=True)
-
-    '''
-    #today = context['execution_date'].day
-    today = datetime.now().day
-    logging.info(last_week)
-    
-    if last_week > today:
-        day_from = 1
-    else:
-        day_from = last_week
-
-    list_ = []
-
-    for day in range(day_from,today+1):
-        filename = conf['temp_data_dir'] + "/SanDiegoData_{0}{1}{2}*.csv".format(cur_yr, cur_mon, day)
-        logging.info("Looking for a file for "+str(day))
-        files = glob.glob(filename)
-        frame = pd.DataFrame()
-        for file_ in files:
-            try:
-                df = pd.read_csv(file_, index_col=False, header=0)
-                list_.append(df)
-                logging.info("Read "+file_)
-            except:
-                logging.info(str(file_) + " is empty or broken") 
-    
-    frame = pd.concat(list_, ignore_index=True)
-
-    '''
 
     # Clean the schema
     logging.info("Cleaning the schema")
@@ -167,7 +138,9 @@ def build_prod_file(**context):
     update = update.sort_values(by='date_trans_start')
 
     logging.info("Removing entries from other years")
-    update = update[update['date_trans_start'] >= cur_yr+'-01-01 00:00:00']
+
+    ### UNCOMMENT THIS FOR PRODUCTION ###
+    #update = update[update['date_trans_start'] >= cur_yr+'-01-01 00:00:00']
 
     # Look for an existing file for the year to add to
     logging.info("Looking for this year's file")
@@ -183,7 +156,6 @@ def build_prod_file(**context):
         logging.info("Starting a new file for this year")
         portal_up = update
         
-
     # Original update number of entries
     orig_entries_ct = portal_up.shape[0]
 
@@ -198,6 +170,9 @@ def build_prod_file(**context):
         ' duplicated entries out of ' +
         str(orig_entries_ct)
         )
+
+    logging.info("Sorting by transaction start date")
+    portal_up_dedupe = portal_up_dedupe.sort_values(by='date_trans_start')
 
     logging.info("Writing updated data to " + portal_fname)
     general.pos_write_csv(
