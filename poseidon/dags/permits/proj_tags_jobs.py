b4b6e828a84943133642c1baae2bbaf06f044689
Andrell Bower <bowera@gmail.com>
2020-08-02 08:00:20 -0700

    fixing origin of project tags files

--- poseidon/dags/permits/proj_tags_jobs.py
+++ poseidon/dags/permits/proj_tags_jobs.py
@@ -29,11 +29,12 @@ def get_tags_file(**context):
 
     fpath = f"P2K_261-Panda_Extract_DSD_Projects_Tags_{filename}.txt"
 
-    command = f"cd {conf['temp_data_dir']} && " \
-    f"curl --user {conf['ftp_datasd_user']}:{conf['ftp_datasd_pass']} " \
-    f"-o {fpath} " \
-    f"ftp://ftp.datasd.org/uploads/dsd/tags/" \
-    f"{fpath} -sk"
+    command = "smbclient //ad.sannet.gov/dfs " \
+        + f"--user={conf['svc_acct_user']}%{conf['svc_acct_pass']} -W ad -c " \
+        + "'prompt OFF;"\
+        + " cd \"DSD-Shared/All_DSD/Panda/\";" \
+        + " lcd \"/data/temp/\";" \
+        + f" get {fpath};'"
 
     command = command.format(quote(command))
 
