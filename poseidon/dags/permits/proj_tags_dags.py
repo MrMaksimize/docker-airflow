5cda8a110a7fe58508efcf5e6b0f377e70faf8fc
Andrell Bower <bowera@gmail.com>
2020-11-02 19:03:01 -0800

    Adding snowflake to dsd proj tags

--- poseidon/dags/permits/proj_tags_dags.py
+++ poseidon/dags/permits/proj_tags_dags.py
@@ -1,16 +1,22 @@
 """DSD Permits subdags file."""
 from airflow.operators.python_operator import PythonOperator
 from trident.operators.s3_file_transfer_operator import S3FileTransferOperator
+from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
 from airflow.models import DAG
 from trident.util import general
 from trident.util.notifications import afsys_send_email
 from dags.permits.proj_tags_jobs import *
-
 from trident.util.seaboard_updates import *
+from trident.util.snowflake_client import *
+
 conf = general.config
 args = general.args
 schedule = general.schedule['dsd_approvals']
 start_date = general.start_date['dsd_approvals']
+
+snowflake_stage = format_stage_sql('dsd_proj_tags')
+snowflake_del = format_delete_sql('dsd_proj_tags')
+snowflake_copy = format_copy_sql('dsd_proj_tags')
 
 dag = DAG(
   dag_id='dsd_proj_tags',
@@ -21,17 +27,15 @@ dag = DAG(
 )
 
 get_file = PythonOperator(
-    task_id='get_tags_files',
-    provide_context=True,
-    python_callable=get_tags_file,
-    
-    dag=dag)
+  task_id='get_tags_files',
+  provide_context=True,
+  python_callable=get_tags_file,
+  dag=dag)
 
 create_prod = PythonOperator(
   task_id="create_tags_prod",
   provide_context=True,
   python_callable=build_tags,
-  
   dag=dag)
 
 upload_file = S3FileTransferOperator(
@@ -42,19 +46,45 @@ upload_file = S3FileTransferOperator(
   dest_s3_conn_id=conf['default_s3_conn_id'],
   dest_s3_key="dsd/permits_set1_project_tags_datasd.csv",
   replace=True,
-  
   dag=dag)
 
 update_tags_md = get_seaboard_update_dag('development-permits-tags.md', dag)
 
 #: Update data inventory json
 update_json_date = PythonOperator(
-    task_id='update_json_date',
-    python_callable=update_json_date,
-    provide_context=True,
-    op_kwargs={'ds_fname': 'development-permits-tags'},
-    
-    dag=dag)
+  task_id='update_json_date',
+  python_callable=update_json_date,
+  provide_context=True,
+  op_kwargs={'ds_fname': 'development-permits-tags'},
+  dag=dag)
+
+stage_snowflake = SnowflakeOperator(
+  task_id=f"stage_snowflake_dsd_proj_tags",
+  sql=snowflake_stage,
+  snowflake_conn_id="snowflake",
+  warehouse="etl_load",
+  database="open_data",
+  schema="public",
+  dag=dag)
+
+delete_snowflake = SnowflakeOperator(
+  task_id=f"del_snowflake_dsd_proj_tags",
+  sql=snowflake_del,
+  snowflake_conn_id="snowflake",
+  warehouse="etl_load",
+  database="open_data",
+  schema="public",
+  dag=dag)
+
+copy_snowflake = SnowflakeOperator(
+  task_id=f"copy_snowflake_dsd_proj_tags",
+  sql=snowflake_copy,
+  snowflake_conn_id="snowflake",
+  warehouse="etl_load",
+  database="open_data",
+  schema="public",
+  dag=dag)
 
 #: Execution rules
 get_file>>create_prod>>upload_file>>[update_tags_md,update_json_date]
+create_prod>>stage_snowflake>>delete_snowflake>>copy_snowflake
