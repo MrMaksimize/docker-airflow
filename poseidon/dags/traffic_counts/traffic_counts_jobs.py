15035ca9ff523e32f509f38401d0fc67c0ac99bc
Andrell Bower <bowera@gmail.com>
2020-11-18 23:43:18 -0800

    updating folder name for traffic counts

--- poseidon/dags/traffic_counts/traffic_counts_jobs.py
+++ poseidon/dags/traffic_counts/traffic_counts_jobs.py
@@ -17,7 +17,7 @@ def get_traffic_counts(out_fname='traffic_counts_file'):
         + f"--user={conf['svc_acct_user']}%{conf['svc_acct_pass']} -W ad -c " \
         + "'cd \"TSW-TEO-Shared/TEO/" \
         + "TEO-Transportation-Systems-and-Safety-Programs/" \
-        + f"Traffic Data/{fy}/RECORD FINDER\";" \
+        + f"Traffic Data/{fy}/Record Finders\";" \
         + f" ls; get Machine_Count_Index.xlsx {conf['temp_data_dir']}/{out_fname}.xlsx;'"
 
     command = command.format(quote(command))
